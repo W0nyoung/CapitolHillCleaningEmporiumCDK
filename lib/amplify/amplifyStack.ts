216eca9a68acc03c99bb3e6338b2651f661b0bc6
W0nyoung <46010070+W0nyoung@users.noreply.github.com>
2024-08-11 15:20:13 -0400

    Updating Prod Route53 stack to lookup existing Public Hosted Zone instead of creating new one. Added JSDocs (#7)

--- lib/amplify/amplifyStack.ts
+++ lib/amplify/amplifyStack.ts
@@ -3,6 +3,11 @@ import { Construct } from 'constructs';
 import { App, GitHubSourceCodeProvider, Platform } from '@aws-cdk/aws-amplify-alpha' 
 import { Config } from '../../bin/config';
 
+/**
+ * Amplify CDK Stack
+ * Creates Amplify App in each account referencing respective GitHub branch depending on stage
+ * GitHub token for UI Repo is stored in AWS Secrets Manager in each account
+ */
 export class AmplifyStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
--- lib/pipeline/pipelineStage.ts
+++ lib/pipeline/pipelineStage.ts
@@ -3,6 +3,11 @@ import { Construct } from 'constructs';
 import { AmplifyStack } from '../amplify/amplifyStack';
 import { Route53Stack } from '../route53/route53Stack';
 
+/**
+ * AWS CodePipeline CDK Stack
+ * Creates a stage for staging and prod
+ * CDK Stacks and their dependency relationships are defined here
+ */
 export class PipelineStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
